33541e41f63f6cf5507ff8c53ea85d92443c51c0
Rafa Morgado <93506265+rafabmorgado@users.noreply.github.com>
2023-11-26 22:31:05 -0300

    Add FoodEntity

---
+++ src/main/java/com/example/cardapio/entities/FoodEntity.java
@@ -0,0 +1,30 @@
+package com.example.cardapio.entities;
+import com.example.cardapio.dtos.FoodRequestDTO;
+import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.*;
+
+@Table(name = "foods")
+@Entity(name = "foods")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class FoodEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String image;
+    private Integer price;
+
+    public FoodEntity(FoodRequestDTO data){
+        this.title = data.title();
+        this.image = data.image();
+        this.price = data.price();
+    }
+}
